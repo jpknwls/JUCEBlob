35d93673826a4c034c64423dc475cf5918ea83c0
jpknwls <28238972+jpknwls@users.noreply.github.com>
2021-05-11 21:34:28 -0700

    Add files via upload

---
+++ register_types.cpp
@@ -0,0 +1,11 @@
+/* register_types.cpp */
+
+#include "register_types.h"
+#include "core/object/class_db.h"
+
+void register_juce_types() {
+}
+
+void unregister_juce_types() {
+   // Nothing to do here in this example.
+}
