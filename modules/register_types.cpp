5f1c32a26d22ad0b44b3132babdb53d86c0f927a
John Knowles <jkn0wles@stanford.edu>
2021-05-11 17:58:09 -0700

    initial file add

---
+++ modules/register_types.cpp
@@ -0,0 +1,12 @@
+/* register_types.cpp */
+
+#include "register_types.h"
+
+#include "core/class_db.h"
+
+void register_summator_types() {
+}
+
+void unregister_summator_types() {
+   // Nothing to do here in this example.
+}
